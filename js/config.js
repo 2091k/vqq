afdc55f017a888c9ed7a78a65f4cb0f4c7464346
2091k <140614254+2091k@users.noreply.github.com>
2025-05-06 16:25:05 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // js/config.js
 // 跨域加速地址（可选，若 API 无需代理可忽略）
-//var PROXY_BASE = 'https://cors.zme.ink/';
-var PROXY_BASE = 'http://jasu.2091k.cn/';
+var PROXY_BASE = 'https://cors.zme.ink/';
+//var PROXY_BASE = 'http://jasu.2091k.cn/';
 
 // 搜索 API 组列表（每组包含关键词搜索和 ID 详情 API）
 var SEARCH_API_GROUPS = [
@@ -10,9 +10,9 @@ var SEARCH_API_GROUPS = [
     keywordApi: 'http://newtv.2091k.cn/?keyword=',
     idApi: 'http://newtv.2091k.cn/?id='
   },
-  {
-    name: '备用站', 
-    keywordApi: 'http://66tyyszy.com/api.php/provide/vod/?ac=videolist&wd=',
-    idApi: 'http://66tyyszy.com/api.php/provide/vod/?ac=detail&ids='
-  }
+//  {
+//    name: '备用站', 
+//    keywordApi: 'http://66tyyszy.com/api.php/provide/vod/?ac=videolist&wd=',
+//    idApi: 'http://66tyyszy.com/api.php/provide/vod/?ac=detail&ids='
+//  }
 ];
